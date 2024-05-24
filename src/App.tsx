4e2896a4109fa0b09398a1c52c68b9e163f18b91
mones <mones.cse@gmail.com>
2024-05-24 19:30:04 +0600

    install react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home.tsx";
+import Landing from "./pages/landing.tsx";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/landing">Landing</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
+      {/* <p>Hello world</p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/landing.tsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <p>This is the landing page</p>
+    </div>
+  );
+};
+export default Landing;
