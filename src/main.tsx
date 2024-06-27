fef4810d19d0050506d40f1573f7d7f70b778577
mones <mones.cse@gmail.com>
2024-06-27 10:58:42 +0600

    install rect-query package

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "@mantine/core/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { MantineProvider } from "@mantine/core";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
+const queryClient = new QueryClient();
 
 if (rootElement) {
 	ReactDOM.createRoot(rootElement).render(
 		<React.StrictMode>
-			<MantineProvider>
-				<Router>
-					<App />
-				</Router>
-			</MantineProvider>
+			<QueryClientProvider client={queryClient}>
+				<MantineProvider>
+					<Router>
+						<App />
+					</Router>
+				</MantineProvider>
+			</QueryClientProvider>
 		</React.StrictMode>,
 	);
 } else {
